10e79762e5992af5f3b05046df44c7de3baf1b17
hiosi123 <tls1641@gmail.com>
2022-03-04 23:18:10 +0900

    final finished

--- final_project/final_project.js
+++ final_project/final_project.js
@@ -26,9 +26,12 @@ pressedBtn = () => {
     token = String(Math.floor(Math.random() * 1000000)).padStart(6,'0');
     document.getElementById('auth__numbers').innerText = token;
     document.getElementById('auth__button').disabled = true;
+    document.getElementById('auth__button').setAttribute('style','color:gray');
     
     let time= 180;
     let timer = null;
+
+    
 
     timer = setInterval(() => {
         if(time >= 0) {
@@ -44,26 +47,66 @@ pressedBtn = () => {
         }
     },1000)
 
+    finshedBtn = () => {
+        clearInterval(timer);
+
+        document.getElementById('auth__finished').disabled = true;
+        document.getElementById('auth__finished').setAttribute('style', 'color: white; background-color:#0068FF')
+       
+       
+    }
+
 }
+
+
+
+
+
+
 
 finshedBtn = () => {
     document.getElementById('auth__finished').disabled = true;
     alert('인증을 완료하셨습니다');
-    document.getElementById('auth__numbers').innerText = '000000'
-    clearInterval(timer);
-    document.getElementById('auth__time').innerText = '00:00'       
-   
+    
 }
 
 function validation() {
-    if (document.getElementById('body__box1').innerText.toString().includes('@') === false) {
-        return ('이메일 주소를 다시 확인해 주세요')
+    const email = document.getElementById('body__box1').value;
+    const name = document.getElementById('body__box2').value;
+    const password1 =  document.getElementById('body__box3').value;
+    const password2 =  document.getElementById('body__box4').value;
+   
+    if (email.toString().includes('@') === false) {
+        document.getElementById('box1__warning').innerText = '이메일을 확인해주세요';
+        document.getElementById('box2__warning').innerText = '';
+        document.getElementById('box3__warning').innerText = '';
+        document.getElementById('box4__warning').innerText = '';
+        return 
+    }  
+    if (name === '') {
+        document.getElementById('box1__warning').innerText = '';
+        document.getElementById('box2__warning').innerText = '이름을 입력해주세요';
+        document.getElementById('box3__warning').innerText = '';
+        document.getElementById('box4__warning').innerText = '';
+        return
     }
-    if (password.length <8 || password.length >16) {
-        return ('비밀번호를 다시 확인해 주세요')
+    if (password1.length <8 || password1.length >16) {
+        document.getElementById('box1__warning').innerText = '';
+        document.getElementById('box2__warning').innerText = '';
+        document.getElementById('box3__warning').innerText = '비밀번호 길이를 확인해주세요';
+        document.getElementById('box4__warning').innerText = '비밀번호 길이를 확인해주세요';
+        return 
     }
-    if (password === passwordCheck === false) {
-        return ('비밀번호가 일치하지 않습니다')
+    if (password1 === password2 === false) {
+        document.getElementById('box1__warning').innerText = '';
+        document.getElementById('box2__warning').innerText = '';
+        document.getElementById('box3__warning').innerText = '비밀번호가 일치하지 않습니다';
+        document.getElementById('box4__warning').innerText = '비밀번호가 일치하지 않습니다';
+        return 
     }
+    document.getElementById('box1__warning').innerText = '';
+    document.getElementById('box2__warning').innerText = '';
+    document.getElementById('box3__warning').innerText = '';
+    document.getElementById('box4__warning').innerText = '';
     alert ('회원가입을 축하합니다')
 }
